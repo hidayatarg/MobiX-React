76b3ae93d221cdbe6a2e45d976d3884b7c7e0be8
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-06-19 13:56:10 +0300

    Created Mobix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { observable } from 'mobx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const appState = observable ({
+    count: 45
+});
+
+const Counter = props =>
+    <div>{props.appState.count}</div>
+
+ReactDOM.render(
+    <Counter appState={appState} />, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
